5c87de1e9c50d32fb4557e725c605b681b6cd1ad
JDavidcor23 <jorgedavid23diaz@gmail.com>
2021-11-17 11:12:02 -0500

    bucles

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let nombrePlanetas = ["Tierra", "Marte", "jupiter"];
 
 let distancia =[5, 3, '67'];
 
-const tamanio = [];
+const tamanio = [10,20,30];
 
 // let i = 0;
 
@@ -33,11 +33,27 @@ const tamanio = [];
 //     }
 // }
 
-for(const key in distancia){
-    if(distancia[key] === '5'){
-        console.log('Se encontró la distancia')
-    }
-    else{
-        console.log('No encontró la distancia')
-    }
-}
+// for(const key in distancia){
+//     if(distancia[key] === '5'){
+//         console.log('Se encontró la distancia')
+//     }
+//     else{
+//         console.log('No encontró la distancia')
+//     }
+// }
+
+// nombrePlanetas.forEach((value, index, array)=>{
+//     console.log('planeta', index, value)
+// })
+
+// nombrePlanetas.map((planetas,index)=>{
+//     return(
+//         console.log(`El planeta ${planetas} tiene tamaño de :${tamanio[index]}`)
+//     )
+// })
+
+let resultadoMap = nombrePlanetas.map((planetas,index)=>{
+     return(
+         console.log(`El planeta ${planetas} tiene tamaño de :${tamanio[index]}`)
+     )
+ })
